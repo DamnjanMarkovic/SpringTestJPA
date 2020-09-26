e689f412a55006141bdab0e12d694f833f0dbc89
damnjan.markovic@hotmail.com <Minhen2020>
2020-09-26 10:59:26 +0200

    first commit

---
+++ src/test/java/com/damnjan/SpringTestJPA/SpringTestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damnjan.SpringTestJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
